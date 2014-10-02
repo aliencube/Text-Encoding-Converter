6c368fddd82da6f5af05a16f21238a1dc548f81f
Justin Yoo <justin.yoo@aliencube.com>
2014-10-02 17:32:24 +1000

    Add WPF app project

---
+++ SourceCodes/01_Applications/TextEncodingConverter.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Navigation;
+
+namespace Aliencube.TextEncodingConverter.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
