d9ce42e1cc1737ceaad0072e5fc98f93cb6503d3
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-24 21:01:50 +0900

    Introduced RelayCommand.

---
+++ SourceCodes/04_Models/TextEncodingConverter.ViewModels/RelayCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace Aliencube.TextEncodingConverter.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event System.EventHandler CanExecuteChanged;
+    }
+}
---
+++ SourceCodes/TextEncodingConverter.ViewModels.Tests/RelayCommandTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Aliencube.TextEncodingConverter.ViewModels;
+using NUnit.Framework;
+
+namespace TextEncodingConverter.ViewModels.Tests
+{
+    [TestFixture]
+    public class RelayCommandTest
+    {
+        [Test]
+        public void SutIsCommand()
+        {
+            var sut = new RelayCommand();
+            Assert.IsInstanceOf<ICommand>(sut);
+        }
+    }
+}
