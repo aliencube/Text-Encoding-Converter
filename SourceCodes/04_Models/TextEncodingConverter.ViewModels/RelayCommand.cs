51fb533a5c455128daae24703d9cfe59a6887fd7
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-24 21:07:10 +0900

    Implemented the OnCanExecute property.

--- SourceCodes/04_Models/TextEncodingConverter.ViewModels/RelayCommand.cs
+++ SourceCodes/04_Models/TextEncodingConverter.ViewModels/RelayCommand.cs
@@ -1,9 +1,22 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 
 namespace Aliencube.TextEncodingConverter.ViewModels
 {
     public class RelayCommand : ICommand
     {
+        private readonly Predicate<object> _onCanExecute;
+
+        public RelayCommand(Predicate<object> onCanExecute, Action<object> onExecute)
+        {
+            this._onCanExecute = onCanExecute;
+        }
+
+        public Predicate<object> OnCanExecute
+        {
+            get { return this._onCanExecute; }
+        }
+
         public bool CanExecute(object parameter)
         {
             throw new System.NotImplementedException();
--- SourceCodes/TextEncodingConverter.ViewModels.Tests/RelayCommandTest.cs
+++ SourceCodes/TextEncodingConverter.ViewModels.Tests/RelayCommandTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using Aliencube.TextEncodingConverter.ViewModels;
 using NUnit.Framework;
 
@@ -10,8 +11,19 @@ namespace TextEncodingConverter.ViewModels.Tests
         [Test]
         public void SutIsCommand()
         {
-            var sut = new RelayCommand();
+            var sut = new RelayCommand(null, null);
             Assert.IsInstanceOf<ICommand>(sut);
+        }
+
+        [Test]
+        public void OnCanExecuteIsCorrect()
+        {
+            Predicate<object> onCanExecute = x => false;
+            var sut = new RelayCommand(onCanExecute, null);
+
+            var actual = sut.OnCanExecute;
+
+            Assert.AreEqual(onCanExecute, actual);
         }
     }
 }
